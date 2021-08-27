cc54f53cec871763ff5203a8eb05996bdbbc1775
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2021-08-27 15:24:21 +0200

    made few changes to Auth footer and product browse footer. Also linked sign in button to sign in page and register button to sign up page

--- frontend/src/components/Navbars/AuthNavbar.js
+++ frontend/src/components/Navbars/AuthNavbar.js
@@ -71,16 +71,29 @@ export default function Navbar(props) {
                 </a>
               </li>
               <li className="flex items-center">
-                <button
+              <Link
+          to="/auth/login"
+          className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+        >
+          Login
+        </Link>
+        <Link
+          to="/auth/register"
+          className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
+          type="button"
+        >
+          Register
+        </Link>
+                {/* <button
                   className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                   type="button">
                    Sign in
-                </button>
-                <button
+                </button> */}
+                {/* <button
                   className="bg-white text-blueGray-700 active:bg-blueGray-50 text-xs font-bold uppercase px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none lg:mr-1 lg:mb-0 ml-3 mb-3 ease-linear transition-all duration-150"
                   type="button">
                    Signup
-                </button>
+                </button> */}
               </li>
             </ul>
           </div>
