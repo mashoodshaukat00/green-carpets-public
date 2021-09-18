caaffd121d5dbe8ad03e91a92158885e1420523b
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2021-09-18 15:24:51 +0200

    basic models and controllers. error in swagger defination

--- backend/GC/GC.WebApi/Services/ProductService.cs
+++ backend/GC/GC.WebApi/Services/ProductService.cs
@@ -9,10 +9,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace GC.WebApi.Services
 {
-    public class ProductService
+    public class ProductService 
     {
         private readonly GreenCarpetsDbContext _context;
-
         public ProductService(GreenCarpetsDbContext context)
         {
             this._context = context;
@@ -32,12 +31,77 @@ namespace GC.WebApi.Services
             return models;
         }
 
+        public async Task<bool> AddAsync(ProductModel model)
+        {
+            var product = MapModelToEntity(model);
+            await _context.Products.AddAsync(product);
+            return await _context.SaveChangesAsync() > 0;                        
+        }
+
+        public async Task<bool> UpdateAsync(ProductModel model)
+        {
+            var result = await _context.Products.FirstOrDefaultAsync(x => x.Id == model.Id);
+            if (result != null)
+            {
+                var entity = MapModelToEntity(model);
+                _context.Products.Update(entity);
+                return await _context.SaveChangesAsync() > 0;
+            }
+            else
+            {
+                throw new Exception("Entry not found in database.");
+            }
+
+        }
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var result = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (result != null)
+            {
+                _context.Products.Remove(result);
+                return await _context.SaveChangesAsync() > 0;
+
+            }
+            else
+            {
+                throw new Exception("Entry not found in database.");
+            }
+        }
+
+        public async Task<ProductModel> GetProductById(int id)
+        {
+            var result = await _context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (result != null)
+            {
+                return MapEntityToModel(result);
+                
+            }
+            else
+            {
+                throw new Exception("Entry not found in database.");
+            }
+        }
+
+        public async Task<ProductModel> GetProductByCategoryId(int id)
+        {
+            var result = await _context.Products.FirstOrDefaultAsync(x => x.CategoryId == id);
+            if (result != null)
+            {
+                return MapEntityToModel(result);
+
+            }
+            else
+            {
+                throw new Exception("Entry not found in database.");
+            }
+        }
+
         private static ProductModel MapEntityToModel(Product product)
         {
             return new ProductModel()
             {
                 Id = product.Id,
-                Name = product.Name,                
+                Name = product.Name,
                 Description = product.Description,
                 CategoryId = product.CategoryId
             };
@@ -48,27 +112,13 @@ namespace GC.WebApi.Services
             return new Product()
             {
                 // Id = model.Id,
-                Name = model.Name,                
+                Name = model.Name,
                 Description = model.Description,
                 CategoryId = model.CategoryId
-                
+
             };
         }
 
-        public async Task<ProductModel> AddAsync(ProductModel model)
-        {
-            var product = MapModelToEntity(model);
-            await _context.Products.AddAsync(product);
-            var results = await _context.SaveChangesAsync();
 
-            if (results > 0)
-            {
-                return MapEntityToModel(product);
-            }
-            else
-            {
-                throw new Exception("Product not added to the database.");
-            }
-        }
     }
 }
