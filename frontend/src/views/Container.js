44a5434433306ff27d4ba3e4731ec21717cdb8be
mashoodshaukat00 <75940719+mashoodshaukat00@users.noreply.github.com>
2021-09-21 02:28:06 +0200

    added data dynamically into the pages and created few components as well

---
+++ frontend/src/views/Container.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import Section from "./Section";
+
+
+export default function Container() {
+    return (
+    <>
+      <main className="profile-page">
+        <Section />
+            <section className="relative py-16 bg-blueGray-200">
+              <div className=" mx-auto px-4">
+              <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
+                <div className="px-6 py-6">
+                <div className="flex flex-wrap justify-center">    
+                             jgljfgjdlfk dlkfjg dfjgdkfg  dfjkkgj
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+    </>
+  );
+}
--- frontend/src/views/browseProduct.js
+++ frontend/src/views/browseProduct.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from "react-router-dom";
-
+import ProductDetail from "./productDetail.js";
 import Navbar from "components/Navbars/AuthNavbar.js";
 import Footer from "components/Footers/Footer.js";
+import Section from "./Section";
 // import { Link } from "heroicons-react";
 
 export default function BrowseProduct() {
@@ -21,45 +22,9 @@ export default function BrowseProduct() {
 
   return (
     <>
-    <Navbar transparent />      
-      {/* <ul>
-      {data.map((item) => (
-        <li key={item.id}>{item.name}</li>
-      ))}
-    </ul>   */}
+    <Navbar transparent />
       <main className="profile-page">
-        <section className="relative block h-500-px">
-          <div
-            className="absolute top-0 w-full h-full bg-center bg-cover"
-            style={{
-              backgroundImage:
-                "url('https://images.unsplash.com/photo-1499336315816-097655dcfbda?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2710&q=80')",
-            }}>
-            <span
-              id="blackOverlay"
-              className="w-full h-full absolute opacity-50 bg-black">                
-            </span>
-          </div>
-          {/* Background ends here */}
-          <div
-            className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
-            style={{ transform: "translateZ(0)" }}>
-            <svg
-              className="absolute bottom-0 overflow-hidden"
-              xmlns="http://www.w3.org/2000/svg"
-              preserveAspectRatio="none"
-              version="1.1"
-              viewBox="0 0 2560 100"
-              x="0"
-              y="0">
-              <polygon
-                className="text-blueGray-200 fill-current"
-                points="2560 0 2560 100 0 100">
-              </polygon>
-            </svg>
-          </div>
-          {/* background bottom ends here */}
-        </section>
+      <Section />
           <section className="relative py-16 bg-blueGray-200">
             <div className=" mx-auto px-4">
              <div className="relative  min-w-0 break-words bg-gray-200 w-full mb-6 shadow-xl rounded-lg -mt-64">
@@ -69,8 +34,8 @@ export default function BrowseProduct() {
                   <div className="flex flex-wrap rounded-lg shadow-lg overflow-hidden -mx-2 md:-mx-1 lg:-mx-2 xl:-mx-2 bg-green">
 
                     {data.map(product =>(
-                      <div className="my-2 px-2 w-1/3 py-6 overflow-hidden sm:w-full md:my-1 md:px-1 md:w-full lg:my-2 lg:px-2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3">
-                      <article className="overflow-hidden rounded-lg shadow-lg border-2 border-solid pt-6">
+                      <div key={product.id} className="my-2 px-2 w-1/3 py-6 overflow-hidden sm:w-full md:my-1 md:px-1 md:w-full lg:my-2 lg:px-2 lg:w-1/3 xl:my-2 xl:px-2 xl:w-1/3">
+                      <article className="overflow-hidden rounded-lg shadow-lg border-2 border-solid border-gray-300 pt-6">
                               <Link to="productDetail">
                                 <a href="#">                            
                                   <img alt="Placeholder" className="block h-auto w-65 px-4 mx-auto rounded-lg" src="https://picsum.photos/600/400/?random"/>
@@ -104,7 +69,7 @@ export default function BrowseProduct() {
                               <button className="flex ml-auto mx-auto mb-4 justify-center text-white bg-blueGray-800 border-0 py-2 px-6 focus:outline-none hover:bg-red-600 rounded">
                                 Buy 
                               </button>
-                            </Link>
+                            </Link>                            
                             </article>
                       </div>
                         )     
